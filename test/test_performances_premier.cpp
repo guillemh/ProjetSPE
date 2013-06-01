880304315a6a76fc1884e0d9e2e508dbd6a02709
Anne-Hermine Allain <anne-hermine.allain@ensimag.imag.fr>
2013-06-01 12:34:54 +0200

    Ajout de tests pour la classe Premier

--- test/test_performances_premier.cpp
+++ test/test_performances_premier.cpp
@@ -12,7 +12,7 @@ void test_performances_premier()
 {
     cout << "-- 1. Test de performances pour les 9592 premiers nombres premiers avec getPremier --" << endl;
 
-     Premier<3> p = Premier<3>();
+    Premier<3> p = Premier<3>();
 
     typedef std::chrono::high_resolution_clock Clock;
     typedef std::chrono::duration<double, std::milli> ms;
@@ -34,4 +34,24 @@ void test_performances_premier()
 	n = p.calculPremier(i);
     t1 = Clock::now();
     std::cout << ms(t1-t0).count() << " millisecond\n";
+
+    cout << "-- 3. Test de performances pour x = 9592 getPremier --" << endl;
+    
+    typedef std::chrono::high_resolution_clock Clock1;
+    typedef std::chrono::duration<double, std::nano> nanos;
+    Clock::time_point t2,t3;
+
+    t2 = Clock1::now();
+    n = p.getPremier(9592);
+    t3 = Clock::now();
+    std::cout << nanos(t3-t2).count() << " nanosecond\n";
+
+    cout << "-- 4. Test de performances pour x = 9592 calculPremier --" << endl;
+    
+    t2 = Clock1::now();
+    n = p.calculPremier(9592);
+    t3 = Clock::now();
+    std::cout << nanos(t3-t2).count() << " nanosecond\n";
+    
+
 }
