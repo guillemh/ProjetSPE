0692c564b70d355258271093ff12460ded9f18a6
Anne-Hermine Allain <anne-hermine.allain@ensimag.imag.fr>
2013-06-03 20:02:55 +0200

    Modif de la skybox : alignement par rapport a la cascade et suppression de la face superieure

--- cascade/skybox.cpp
+++ cascade/skybox.cpp
@@ -6,10 +6,9 @@ using namespace std;
 
 Skybox::Skybox(const Cascade<3> *c)
 {
-    box_x = c->getBassinY() * 10;
-    box_y = c->getBassinX() * 10;
+    box_x = c->getBassinY() * 20;
+    box_y = c->getBassinX() * 20;
     box_z = c->getBassinZ();
-    int dimension = 2*box_x;
 }
 
 Skybox::~Skybox() {}
@@ -20,52 +19,52 @@ void Skybox::draw()
     
  
     glBegin(GL_QUADS);
-    // *box_xace avant
-    glNormal3d(0, 0, 1);
-    glVertex3d(0.5*box_x, -0.5*box_x, 0.5*box_x);
-    glVertex3d(0.5*box_x, 0.5*box_x, 0.5*box_x);
-    glVertex3d(-0.5*box_x, 0.5*box_x, 0.5*box_x);
-    glVertex3d(-0.5*box_x, -0.5*box_x, 0.5*box_x);
- 
-    glEnd();
- 
-    glBegin(GL_QUADS);
     // face droite
+    glNormal3d(0, 0, 1);
+    glVertex3f(-0.5*box_x, 0.5*box_x, 1*box_x);
+    glVertex3f(0.5*box_x, 0.5*box_x, 1*box_x);
+    glVertex3f(0.5*box_x, 0.5*box_x, 0);
+    glVertex3f(-0.5*box_x, 0.5*box_x, 0);
+ 
+    glEnd();
+ 
+    glBegin(GL_QUADS);
+    // face avant
     glNormal3d(1, 0, 0);
-    glVertex3d(0.5*box_x, 0.5*box_x, -0.5*box_x);
-    glVertex3d(0.5*box_x, 0.5*box_x, 0.5*box_x);
-    glVertex3d(0.5*box_x, -0.5*box_x, 0.5*box_x);
-    glVertex3d(0.5*box_x, -0.5*box_x, -0.5*box_x);
- 
-    glEnd();
- 
-    glBegin(GL_QUADS);
-    // face gauche
-    glNormal3d(-1, 0, 0);
-    glVertex3d(-0.5*box_x, -0.5*box_x, 0.5*box_x);
-    glVertex3d(-0.5*box_x, 0.5*box_x, 0.5*box_x);
-    glVertex3d(-0.5*box_x, 0.5*box_x, -0.5*box_x);
-    glVertex3d(-0.5*box_x, -0.5*box_x, -0.5*box_x);
- 
-    glEnd();
- 
-    glBegin(GL_QUADS);
-    // dessous
-    glNormal3d(0, -1, 0);
-    glVertex3d(0.5*box_x, -0.5*box_x, 0.5*box_x);
-    glVertex3d(-0.5*box_x, -0.5*box_x, 0.5*box_x);
-    glVertex3d(-0.5*box_x, -0.5*box_x, -0.5*box_x);
-    glVertex3d(0.5*box_x, -0.5*box_x, -0.5*box_x);
+    glVertex3d(0.5*box_x, 0.5*box_x, 0);
+    glVertex3d(0.5*box_x, 0.5*box_x, box_x);
+    glVertex3d(0.5*box_x, -0.5*box_x, box_x);
+    glVertex3d(0.5*box_x, -0.5*box_x, 0);
  
     glEnd();
  
     glBegin(GL_QUADS);
     // face arriere
+    glNormal3d(-1, 0, 0);
+    glVertex3d(-0.5*box_x, -0.5*box_x, box_x);
+    glVertex3d(-0.5*box_x, 0.5*box_x, box_x);
+    glVertex3d(-0.5*box_x, 0.5*box_x, 0);
+    glVertex3d(-0.5*box_x, -0.5*box_x, 0);
+ 
+    glEnd();
+ 
+    glBegin(GL_QUADS);
+    // face gauche
+    glNormal3d(0, -1, 0);
+    glVertex3d(0.5*box_x, -0.5*box_x, box_x);
+    glVertex3d(-0.5*box_x, -0.5*box_x, box_x);
+    glVertex3d(-0.5*box_x, -0.5*box_x, 0);
+    glVertex3d(0.5*box_x, -0.5*box_x, 0);
+ 
+    glEnd();
+ 
+    glBegin(GL_QUADS);
+    // face dessous
     glNormal3d(0, 0, -1);
-    glVertex3d(0.5*box_x, 0.5*box_x, -0.5*box_x);
-    glVertex3d(0.5*box_x, -0.5*box_x, -0.5*box_x);
-    glVertex3d(-0.5*box_x, -0.5*box_x, -0.5*box_x);
-    glVertex3d(-0.5*box_x, 0.5*box_x, -0.5*box_x);
+    glVertex3d(0.5*box_x, 0.5*box_x, 0);
+    glVertex3d(0.5*box_x, -0.5*box_x, 0);
+    glVertex3d(-0.5*box_x, -0.5*box_x, 0);
+    glVertex3d(-0.5*box_x, 0.5*box_x, 0);
     
     glEnd();
     
