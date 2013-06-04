f6a1138778e26f6c00b096b31f7ca1b340c4d73a
loic ciccone <cicconel@ensimag.imag.fr>
2013-06-04 18:19:14 +0200

    fonction coloriage de metaballs et definition de l'isosurface des particules

--- metaballs/metaballs.cpp
+++ metaballs/metaballs.cpp
@@ -1,15 +1,36 @@
 #include "metaballs.hpp"
 
+
+/* ** Constructeurs ** */
+
 Metaballs::Metaballs() {
 
 }
 
 Metaballs::~Metaballs() {
-
 }
 
-void Metaballs::coloration(const list<Particule <Dim>* > &particules);
 
+/* ** Methodes ** */
+
+void Metaballs::coloration(const list<Particule<3> *> &particules) {
+    typename list<Particule<Dim> *>::iterator it;
+
+    // Pour chaque point, on regarde l'influence des particules du fluide
+    // Si elle superieure a rayon, on colorie le point
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < p; j++) {
+            for (int k = 0; k < q; k++) {
+                Vecteur<3> position = origine + Vecteur<3>(i*cote, j*cote, k*cote);
+                
+                double influence = 0;
+                for (it = particules.begin(); it != particules.end(); it++)
+                    influence += (*it)->isosurface(position);
+                    
+                points[i][j][k] = (influence >= rayon)? true : false;
+            }
+        }
+    }
 }
 
 
@@ -17,9 +38,11 @@ void Metaballs::draw() {
 
 }
 
+
 void Metaballs::drawCube(Vecteur<Dim> pos, double cote, int config) {
 
 }
+
 
 void Metaballs::drawTriangle(Vecteur<Dim> pos, double cote, int a, int b, int c) {
 
