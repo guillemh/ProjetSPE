2868fcee465636ce5a01a6fd9de0d401a219ef1b
Jean-Philippe Bauchet (PC perso) <Jean-Philippe.Bauchet@ensimag.imag.fr>
2013-06-06 00:31:44 +0200

    Mise en place d'un test pour Metaballs : vérifiction des configurations

---
+++ metaballs/scene.h
@@ -0,0 +1,37 @@
+#ifndef _SCENE_
+#define _SCENE_
+
+#include <GL/glut.h>
+#include "metaballs.hpp"
+#include <vector>
+
+using std::vector;
+
+/**
+ * \class Scene Classe contenant les objets représentés sur la scène
+ */
+class Scene {
+ public :
+    /**
+     * \brief Constructeur
+     *
+     * Constructeur par défaut d'un objet de classe Scène
+     */
+    Scene () ;
+
+    /**
+     * \brief Destructeur
+     */
+    ~Scene () ;
+
+    /**
+     * \brief Représentation
+     *
+     * Représentation de la scène
+     */
+    void draw() ;
+
+ private :
+    vector<Metaballs*> v ;
+};
+#endif
