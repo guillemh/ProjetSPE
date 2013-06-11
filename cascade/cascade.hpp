008eab9c7e8133ef82f5ea57b0212297a6f2d7ab
Anne-Hermine Allain <anne-hermine.allain@ensimag.imag.fr>
2013-06-11 12:07:10 +0200

    Suppression des attributs et methodes inutiles dans cascade

--- cascade/cascade.hpp
+++ cascade/cascade.hpp
@@ -24,14 +24,6 @@ private :
     double bassin_x;      /*!< Dimensions de la cuve rectangulaire selon l'axe (Ox) */
     double bassin_y;      /*!< Dimensions de la cuve rectangulaire selon l'axe (Oy) */
     double bassin_z;      /*!< Dimensions de la cuve rectangulaire selon l'axe (Oz) */
-    double obstacle_x;    /*!< Dimensions de l'obstacle rectangulaire selon l'axe (Ox) */
-    double obstacle_y;    /*!< Dimensions de l'obstacle rectangulaire selon l'axe (Oy) */
-    double obstacle_z;    /*!< Dimensions de l'obstacle rectangulaire selon l'axe (Oz) */
-    double pente_angle;   /*!< Angle formé par la pente et l'horizontale */
-    double pente_x;       /*!< Longueur totale de la pente */
-    double pente_y;       /*!< Largeur de la pente */
-    double pente_z;       /*!< Hauteur des bords de la pente */
-
     bool arps;   /* pour choisir entre l'arps ou pas pour l'animation */
 
 public :
@@ -55,17 +47,8 @@ public :
      * \param _bassin_x Dimension selon l'axe (Ox) du bassin
      * \param _bassin_y Dimension selon l'axe (Oy) du bassin
      * \param _bassin_z Dimension selon l'axe (Oz) du bassin
-     * \param _obstacle_x Dimension selon l'axe (Ox) de l'obstacle
-     * \param _obstacle_y Dimension selon l'axe (Oy) de l'obstacle 
-     * \param _obstacle_z Dimension selon l'axe (Oz) de l'obstacle
-     * \param _pente_angle Angle formé par la pente par rapport au plan horizontal
-     * \param _pente_x Longueur de la pente
-     * \param _pente_y Largeur de la pente
-     * \param _pente_z Hauteur pour les bords de la pente
      */
-    Cascade(Fluide<Dim> *_f, double _bassin_x, double _bassin_y, double _bassin_z,
-	    double _obstacle_x, double _obstacle_y, double _obstacle_z, 
-	    double _pente_angle, double _pente_x, double _pente_y, double _pente_z);
+    Cascade(Fluide<Dim> *_f, double _bassin_x, double _bassin_y, double _bassin_z);
 
     /**
      * \brief Destructeur
@@ -110,48 +93,6 @@ public :
    * \return La position en z du bassin
    */
   const double & getBassinZ() const;
-
-  /**
-   * @brief Accesseur
-   * \return La position en x de l'obstacle
-   */
-  const double & getObstacleX() const; 
-
-  /**
-   * @brief Accesseur
-   * \return La position en y de l'obstacle
-   */
-  const double & getObstacleY() const;   
-
-  /**
-   * @brief Accesseur
-   * \return La position en z de l'obstacle
-   */
-  const double & getObstacleZ() const;   
-
-  /**
-   * @brief Accesseur
-   * \return L'angle de la pente
-   */
-  const double & getPenteAngle() const;   
-
-  /**
-   * @brief Accesseur
-   * \return La position en x de la pente
-   */
-  const double & getPenteX() const;       
-
-  /**
-   * @brief Accesseur
-   * \return La position en y de la pente
-   */
-  const double & getPenteY() const;   
-
-  /**
-   * @brief Accesseur
-   * \return La position en z de la pente
-   */
-  const double & getPenteZ() const;   
 };
 
 #include "cascade.tpp"
